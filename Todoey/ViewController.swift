8aa247c70e49a873af9895e5ac738a735609366a
박민수 <plmspk@naver.com>
2018-09-20 23:31:43 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by MIN SU PARK on 2018. 9. 20..
+//  Copyright © 2018년 MIN SU PARK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
